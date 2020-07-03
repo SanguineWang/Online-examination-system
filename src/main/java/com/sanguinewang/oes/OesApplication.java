3ff356b4a56afbeb452b43f0eed25859364185ba
SanguineWang <1443133579@qq.com>
2020-07-03 09:46:59 +0800

    init

---
+++ src/main/java/com/sanguinewang/oes/OesApplication.java
@@ -0,0 +1,13 @@
+package com.sanguinewang.oes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sanguinewang/oes/OesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanguinewang.oes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
